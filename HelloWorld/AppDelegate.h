e3fce2bef79324fe1ad2dcd6cadbb763cf7033f5
abaobaocom <78615@qq.com>
2015-09-24 17:23:25 +0800

    2015-09-24

---
+++ HelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by tonymacmini on 14/11/15.
+//  Copyright (c) 2014年 tonyguan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by tonymacmini on 14/11/15.
+//  Copyright (c) 2014年 tonyguan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
